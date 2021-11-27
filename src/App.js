5e42b2ce65dedcba35620407bd965a7f28cf9981
onlymg <me@onlymg.com>
2021-11-27 15:47:07 +0530

    Added the cart button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Layout/Header";
+
+import React from "react";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
 
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import mealsImage from '../../assets/meals.jpg';
+
+import HeaderCartButton from './HeaderCartButton';
+
+import classes from './Header.module.css';
+
 const Header = props => {
-    
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>Gill Dhaba</h1>
+            <HeaderCartButton>Cart</HeaderCartButton>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table full of delicious food!" />
+        </div>
+    </React.Fragment>
 };
 
 export default Header;
