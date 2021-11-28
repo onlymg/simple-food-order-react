0428128f38e693bd3bea293f93b04bf1ac10960a
onlymg <me@onlymg.com>
2021-11-28 20:40:18 +0530

    Add the meals list and description with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Layout/Header";
 
+import Meals from "./components/Meals/Meals";
+
 import React from "react";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
