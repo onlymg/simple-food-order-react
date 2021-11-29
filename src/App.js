e4c8ab481102b1c9940737fc462de099f94d5a81
onlymg <me@onlymg.com>
2021-11-29 12:49:44 +0530

    Added the Cart component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Header from "./components/Layout/Header";
 
 import Meals from "./components/Meals/Meals";
 
+import Cart from "./components/Cart/Cart";
+
 import React from "react";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import classes from './Cart.module.css';
 
+import Modal from '../UI/Modal';
+
 const Cart = props => {
     const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
 
     return (
-        <div>
+        <Modal>
             {cartItems} 
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -14,7 +16,7 @@ const Cart = props => {
                 <button className={classes['button-alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
