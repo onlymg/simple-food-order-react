10e0b47e0aa10d2841dbd14967ace52a687d4bc7
onlymg <me@onlymg.com>
2021-11-29 12:19:15 +0530

    added the MealItemForm component

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,5 +1,7 @@
 import classes from './MealItem.module.css';
 
+import MealItemForm from './MealItemForm';
+
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
     
@@ -10,7 +12,7 @@ const MealItem = props => {
             <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+            <MealItemForm id={props.id}/>
         </div>
     </li>
 };
